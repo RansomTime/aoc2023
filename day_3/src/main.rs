c8998b4b7d84d6258f28a9a3f9f43a569d861d53
Alex Bryson <alex@ransomti.me>
2023-12-03 12:51:18 +0000

    day 3

---
+++ day_3/src/main.rs
@@ -0,0 +1,209 @@
+fn file_to_string(path: &str) -> String {
+  use std::fs::File;
+  use std::io::prelude::*;
+  let mut file = File::open(path).unwrap();
+  let mut contents = String::new();
+  file.read_to_string(&mut contents).unwrap();
+  if contents.ends_with('\n') {
+    contents.pop();
+    if contents.ends_with('\r') {
+      contents.pop();
+    }
+  }
+  contents
+}
+
+fn main() {
+  println!("Part 1: {}", part_1(file_to_string("inputs/input")));
+  println!("Part 2: {}", part_2(file_to_string("inputs/input")));
+}
+
+#[derive (Debug, Copy, Clone, PartialEq)]
+struct Num {
+  start: usize,
+  end: usize,
+  value: i32,
+}
+
+impl Num {
+  fn get_neighbours(&self, row_size: usize) -> Vec<usize>{
+    let mut res: Vec<usize> = vec![];
+    let left = match self.start.checked_sub(1) {
+      Some(l) => {
+        res.push(l);
+        l
+      },
+      None => self.start,
+    };
+    let right = self.end + 1;
+    res.push(right);
+    if let Some(in_range) = left.checked_sub(row_size) {
+      res.append(&mut (in_range..=right-row_size).collect::<Vec<usize>>());
+    }; //go up if we're in range, if not then we're at the top of the string
+
+    res.append(&mut(left+row_size..=right+row_size).collect::<Vec<usize>>());
+    // don't need to worry about oob here, as we're just comparing to the list
+    // of symbol positions
+    res
+
+  }
+}
+
+#[derive(PartialEq)]
+enum Symbol {
+  Dot,
+  Number,
+  Newline,
+  Symbol,
+  Gear,
+}
+
+impl Symbol {
+  fn from_char(input: char) -> Symbol {
+    match input {
+      '.' => Symbol::Dot,
+      '0'..='9' => Symbol::Number,
+      '\n' => Symbol::Newline,
+      '*' => Symbol::Gear,
+      _ => Symbol::Symbol,
+    }
+  }
+}
+
+
+
+fn part_1(input: String) -> i32 {
+
+  let mut start_num: Option<usize> = None;
+  let mut symbols: Vec<usize> = vec![];
+  let mut nums: Vec <Num> = vec![];
+  let row_size = input.split('\n').next().unwrap().chars().count() + 1;
+
+  for (pos, symbol) in input.chars().enumerate() {
+    let symbol_type = Symbol::from_char(symbol);
+    if symbol_type == Symbol::Symbol || symbol_type == Symbol::Gear {
+      symbols.push(pos)
+    }
+    if let Some(start) = start_num {
+      if Symbol::from_char(symbol) != Symbol::Number {
+        nums.push(
+          Num {
+            start,
+            end: pos-1,
+            value: input[start..pos].parse::<i32>().unwrap(),
+          }
+        );
+        start_num = None;
+      }
+    } else if Symbol::from_char(symbol) == Symbol::Number {
+      start_num = Some(pos);
+    }
+  }
+
+  let mut res = 0;
+
+  for num in nums {
+    let mut neighbours = num.get_neighbours(row_size);
+    neighbours.retain(|e| symbols.contains(e));
+    if !neighbours.is_empty() { // next to symbol
+      res += num.value;
+    }
+  }
+
+
+  res
+}
+
+struct Gear {
+  position: usize,
+  parts: Vec<i32>,
+}
+
+impl Gear {
+  fn is_valid_gear(&self) -> bool {
+    self.parts.len() == 2
+  }
+
+  fn get_ratio(&self) -> i32 {
+    match self.is_valid_gear() {
+      false => 0,
+      true => {
+        self.parts.first().unwrap() * self.parts.last().unwrap()
+      }
+    }
+  }
+}
+
+fn part_2(input: String) -> i32 {
+
+  let mut symbols: Vec<usize> = vec![];
+  let mut gears: Vec<Gear> = vec![];
+  let mut gear_pos = vec![];
+  let mut nums: Vec <Num> = vec![];
+  let row_size = input.split('\n').next().unwrap().chars().count() + 1;
+  let mut start_num: Option<usize> = None;
+
+  for (pos, symbol) in input.chars().enumerate() {
+    let symbol_type = Symbol::from_char(symbol);
+    if symbol_type == Symbol::Symbol {
+      symbols.push(pos);
+    }
+    if symbol_type == Symbol::Gear {
+      symbols.push(pos);
+      gears.push(Gear {position: pos, parts: vec![]});
+      gear_pos.push(pos);
+    }
+    if let Some(start) = start_num {
+      if Symbol::from_char(symbol) != Symbol::Number {
+        nums.push(
+          Num {
+            start,
+            end: pos-1,
+            value: input[start..pos].parse::<i32>().unwrap(),
+          }
+        );
+        start_num = None;
+      }
+    } else if Symbol::from_char(symbol) == Symbol::Number {
+      start_num = Some(pos);
+    }
+  }
+
+  for num in nums {
+    let mut neighbour_gears = num.get_neighbours(row_size);
+    neighbour_gears.retain(|e| gear_pos.contains(e));
+
+    for neighbour_gear in neighbour_gears {
+      for gear in &mut gears {
+        if gear.position == neighbour_gear {
+          gear.parts.push(num.value);
+        }
+      }
+    }
+  }
+  let mut res = 0;
+  for gear in gears {
+    res += gear.get_ratio();
+  }
+  res
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_part_1() {
+    assert_eq!(part_1(file_to_string("inputs/demo")), 4361);
+  }
+
+  #[test]
+  fn test_part_2() {
+    assert_eq!(part_2(file_to_string("inputs/demo")), 467835);
+  }
+
+  #[test]
+  fn test_incorrect_answers() {
+  }
+}
